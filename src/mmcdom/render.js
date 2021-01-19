16902f5d787d81a4195ffed3b95e1b6a99fff528
Erick-34 <erickoliveira2060@gmail.com>
2021-01-19 20:08:44 -0300

    add: render components

--- src/mmcdom/render.js
+++ src/mmcdom/render.js
@@ -1,7 +1,6 @@
 import mount from "./mount";
-import patch from "./patch";
 
-function render(element, nextElement, container) {
+function render(element, container) {
   if (!container) {
     throw new Error(
       "O container não existe. Você precisa passar um container para renderizar seu app."
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,27 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   devServer: {
     port: 9000,
-    contentBase: './public'
+    contentBase: "./public",
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  entry: './src/index.js',
+  entry: { index: "./src" },
   module: {
     rules: [
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-react-jsx', 
-            'babel-plugin-transform-react-jsx']
+            presets: ["@babel/preset-env"]
           },
-          
-        }
-      }
-    ]
-  }
-}
+        },
+      },
+    ],
+  },
+};
